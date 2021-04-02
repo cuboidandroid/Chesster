d83a1f07efbae780ae58ad9e4e775c078ab10816
Stanisław Puchała <stanley@mba2020m1.local>
2021-04-02 23:21:56 +0200

    pieces moves, takebacks/undo, highlighting selected, game notation/recording moves

--- pyfiles/game_main.py
+++ pyfiles/game_main.py
@@ -1,7 +1,7 @@
 import pygame as p
-from pyfiles import game_session
+from pyfiles.game_session import *
 
-width = height = 512
+width = height = 640
 dimension = 8
 sq_size = height // dimension
 max_fps = 15
@@ -17,9 +17,9 @@ def load_images():
 def main():
     p.init()
     screen = p.display.set_mode((width, height))
-    clock = p.time.Clock
+    # clock = p.time.Clock
     screen.fill(p.Color("white"))
-    gs = game_session.GameSession()
+    gs = GameSession()
     load_images()
     running = True
     sq_selected = ()  # last click of a user
@@ -28,27 +28,41 @@ def main():
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+                print('\n\nNICE GAME:\n\n')
+                [print(move) for move in gs.notation]
 
             elif e.type == p.MOUSEBUTTONDOWN:
-                location = p.mouse.get_pos()
-                col = location[0] // sq_size
-                row = location[1] // sq_size
-                if sq_selected == (row, col):  # twice clicked same square
+                l_click, m_click, r_click = p.mouse.get_pressed(3)
+                if l_click:
+                    location = p.mouse.get_pos()
+                    col = location[0] // sq_size
+                    row = location[1] // sq_size
+                    if sq_selected == (row, col):  # twice clicked same square
+                        sq_selected = ()  # deselect
+                        player_clicks = []
+                    else:
+                        sq_selected = (row, col)
+                        player_clicks.append(sq_selected)
+                    if len(player_clicks) == 2:
+                        move = Move(player_clicks[0], player_clicks[1], gs.board)
+                        print(move.generate_move_notation())
+                        gs.make_move(move)
+                        sq_selected = ()  # deselect
+                        player_clicks = []
+
+                elif r_click:
                     sq_selected = ()  # deselect
                     player_clicks = []
-                else:
-                    sq_selected = (row, col)
-                    player_clicks.append(sq_selected)
-                if len(player_clicks) == 2:
-                    pass
+                    gs.undo_move()
 
-        draw_game(screen, gs)
+        draw_game(screen, gs, player_clicks)
         # clock.tick(max_fps)
         p.display.flip()
 
 
-def draw_game(screen, gs):
+def draw_game(screen, gs, fields):
     draw_board(screen)
+    draw_selected(screen, fields)
     draw_pieces(screen, gs.board)
 
 
@@ -63,28 +77,14 @@ def draw_board(screen):
 def draw_pieces(screen, board):
     for r in range(dimension):
         for c in range(dimension):
-            piece = piece_name(board[r, c])
+            piece = GameSession.piece_name(board[r, c])
             if piece != "--":
                 screen.blit(images[piece], p.Rect(c*sq_size, r*sq_size, sq_size, sq_size))
 
 
-def piece_name(n):
-    d = {
-        0: "--",
-        -10: "bp",
-        10: "wp",
-        -30: "bN",
-        30: "wN",
-        -32: "bB",
-        32: "wB",
-        -50: "bR",
-        50: "wR",
-        -90: "bQ",
-        90: "wQ",
-        -99: "bK",
-        99: "wK"
-    }
-    return d[n]
+def draw_selected(screen, fields):
+    for field in fields:
+        p.draw.rect(screen, p.Color(242, 153, 244), p.Rect(field[1] * sq_size, field[0] * sq_size, sq_size, sq_size))
 
 
 if __name__ == '__main__':
--- pyfiles/game_session.py
+++ pyfiles/game_session.py
@@ -1,5 +1,4 @@
 import numpy as np
-from game_main import piece_name
 
 
 class GameSession:
@@ -11,6 +10,8 @@ class GameSession:
         self.black_can_castle_long = True
         self.black_can_castle_short = True
         self.white_to_move = True
+        self.notation = []
+        self.history = []
 
     @staticmethod
     def startup_board():
@@ -21,21 +22,80 @@ class GameSession:
         board[1][:] = -board[6][:]
         return board
 
-    class Move:
+    def validate_move(self, move):
+        conditions = []
 
-        def __init__(self, from_sq, to_sq, board):
-            self.from_sq = from_sq
-            self.to_sq = to_sq
+        # who is on the move
 
-            self.moved_piece = piece_name(board[from_sq])
-            self.destination_field = piece_name(board[to_sq])
+        if self.white_to_move and move.moved_piece > 0:
+            conditions.append(True)
+        elif not self.white_to_move and move.moved_piece < 0:
+            conditions.append(True)
+        else:
+            conditions.append(False)
 
-        def chess_note(self, from_sq, to_sq, moved_piece):
-            files = 'abcdefgh'
-            st_row = 7 - from_sq[0]+1
-            st_col = files[from_sq[1]]
+        return all(conditions)
 
-            to_row = 7 - to_sq[0]+1
-            to_col = files[to_sq[1]]
+    def make_move(self, move):
+        if self.validate_move(move):
+            self.board[move.from_sq] = 0
+            self.board[move.to_sq] = move.moved_piece
+            self.history.append(move)
+            self.update_notation(move)
+            self.white_to_move = not self.white_to_move
 
-            return moved_piece + " : " + str(st_col) + str(st_row) + " - " + str(to_col) + str(to_row)
+    def update_notation(self, move):
+        if move:
+            self.notation.append(move.generate_move_notation())
+        else:
+            self.notation.pop()
+
+    def undo_move(self):
+        if self.history:
+            self.update_notation(None)
+
+            last_move = self.history.pop()
+            self.board[last_move.to_sq] = last_move.destination_field
+            self.board[last_move.from_sq] = last_move.moved_piece
+            self.white_to_move = not self.white_to_move
+        else:
+            pass
+
+    @staticmethod
+    def piece_name(n):
+        d = {
+            0: "--",
+            -10: "bp",
+            10: "wp",
+            -30: "bN",
+            30: "wN",
+            -32: "bB",
+            32: "wB",
+            -50: "bR",
+            50: "wR",
+            -90: "bQ",
+            90: "wQ",
+            -99: "bK",
+            99: "wK"
+        }
+        return d[n]
+
+
+class Move:
+
+    def __init__(self, from_sq, to_sq, board):
+        self.from_sq = from_sq
+        self.to_sq = to_sq
+
+        self.moved_piece = board[from_sq]
+        self.destination_field = board[to_sq]
+
+    def generate_move_notation(self):
+        files = 'abcdefgh'
+        # st_row = 7 - self.from_sq[0]+1
+        # st_col = files[self.from_sq[1]]
+
+        to_row = 7 - self.to_sq[0]+1
+        to_col = files[self.to_sq[1]]
+
+        return GameSession.piece_name(self.moved_piece)[1:] + ' ' + str(to_col) + str(to_row)
